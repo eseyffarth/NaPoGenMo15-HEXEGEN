5c28d8880ec39cd6a0d90929bab3c64fa5cb212d
Esther Seyffarth <esther.seyffarth@gmail.com>
2015-04-09 22:07:45 +0200

    added dactyl/spondee alternation

--- hexegen.py
+++ hexegen.py
@@ -1,33 +1,37 @@
 __author__ = 'Esther Seyffarth'
 
 
-from nltk.corpus import cmudict
+from nltk.corpus import cmudict, gutenberg
 import re
 import random
 
-
 words = cmudict.dict()
 to_delete = []
-success = 0
-error = 0
+
 for word in words:
     try:
         words[word] = "".join(*words[word])
-        success += 1
     except TypeError:
         # TODO: handle this better
         #print("couldn't generate dictionary entry")
         to_delete.append(word)
-        error += 1
 
 for item in to_delete:
     del words[item]
 
-
+re.VERBOSE
 
 dactyl = re.compile("^[A-Z]+(1|2)[A-Z]+0[A-Z]+0[A-Z]*$")
 spondee = re.compile("^[A-Z]+(1|2)[A-Z]+(1|2)[A-Z]*$")
 final = re.compile("^[A-Z]+(1|2)[A-Z]+[0-9][A-Z]*$")
+
+verse = re.compile("""^([A-Z]+(1|2)[A-Z]+0[A-Z]+0[A-Z]*)|([A-Z]+(1|2)[A-Z]+(1|2)[A-Z]*){4}    # first 4 feet are dactyl or spondee
+                   [A-Z]+(1|2)[A-Z]+0[A-Z]+0[A-Z]*              # fifth foot is a dactyl
+                   [A-Z]+(1|2)[A-Z]+[0-9][A-Z]*$                # sixth foot has two syllables
+                   """
+)
+
+one_syllable = re.compile("^[^aeiou]*[aeiou]+[^aeiou]*$")
 
 dactyl_words = set()
 spondee_words = set()
@@ -39,7 +43,6 @@ for word in words:
         spondee_words.add(word)
     elif re.match(final, words[word]):
         final_words.add(word)
-
 
 def make_verse():
     verse = []
@@ -59,6 +62,31 @@ def make_poem():
     for i in range(50):
         poem.append(make_verse())
     return "\n".join(poem)
+
+def get_poem():
+    """
+    This function should extract hexametric sentences from Gutenberg texts, but it doesn't.
+    Either hexametric sentences are too rare, or the absence of basic function words from CMUdict results in problems
+    with the matching of the whole sentence.
+    """
+    outtext = []
+    for corpus in gutenberg.fileids():
+        text = gutenberg.sents(corpus)
+        for sentence in text:
+            transcription = ""
+            discard = False
+            for word in sentence:
+                if word.lower() in words:
+                    transcription += words[word.lower()]
+                elif re.match(one_syllable, word.lower()):
+                    # consider this word a "small", unstressed word
+                    transcription += "A0A"
+                else:
+                    discard = True
+            if re.match(verse, transcription) and not discard:
+                print(sentence, transcription)
+                outtext.append(" ".join(sentence))
+    return "\n".join(outtext)
 
 outpath = input("please specify output path: ")
 outpath = re.sub("\\\\", "/", outpath)
